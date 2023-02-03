31445f0d70fcb58b91f461e9ee14226822f3cd09
anyepeter <petzyrockchendi@gmail.com>
2023-02-03 08:58:45 +0100

    fix linter errors

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 def decode(text)
-  morse_code = {
+  morse_text = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
     '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
     '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W',
@@ -7,8 +7,8 @@ def decode(text)
   }
   morse_array = text.split(/(\s)/)
   morse_decode = []
-  morse_array.each do |morse|
-    morse_decode.push(morse_code[morse])
+  morse_array.each do |code|
+    morse_decode.push(morse_text[code])
   end
   morse_decode.join
 end
